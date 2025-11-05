8da69b289356cb916804f2aa02f7899745013d1e
Sandeep Sabharwal <sabharwalsandeep989@gmail.com>
2025-11-05 21:43:10 +0530

    Create day35 que2.c

---
+++ day35 que2.c
@@ -0,0 +1,30 @@
+# Rotate an array to the right by k positions
+
+#include <stdio.h>
+
+void reverse(int arr[], int start, int end) {
+    while(start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = 2;
+
+    k = k % n;
+    reverse(arr, 0, n - 1);
+    reverse(arr, 0, k - 1);
+    reverse(arr, k, n - 1);
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
